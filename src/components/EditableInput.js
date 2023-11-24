2742988882a5ea4143c41d1c052023e03def3120
AkashRana18 <akashmanu2003@gmail.com>
2023-11-24 20:50:58 +0530

    linked google and facebook to one account , created provider block

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -24,13 +24,13 @@ const EditableInput = ({
   const onSaveClick = async () => {
     const trimmed = input.trim();
     if (trimmed === '') {
-        Alert.info(emptyMsg, 4000);
+      Alert.info(emptyMsg, 4000);
     }
     if (trimmed !== initialValue) {
       await onSave(trimmed);
     }
     setIsEditable(false);
-  }
+  };
 
   return (
     <div>
@@ -43,11 +43,11 @@ const EditableInput = ({
           value={input}
           onChange={onInputChange}
         />
-        <InputGroup.Button>
+        <InputGroup.Button onClick={onEditClick}>
           <Icon icon={isEditable ? 'close' : 'edit2'} />
         </InputGroup.Button>
         {isEditable && (
-          <InputGroup.Button onClick= {onSaveClick}>
+          <InputGroup.Button onClick={onSaveClick}>
             <Icon icon="check" />
           </InputGroup.Button>
         )}
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,22 @@
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
-  const onSave =  newData => {
-    console.log(newData)
-  }
-
+  const onSave = async newData => {
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
 
   return (
     <>
@@ -17,11 +26,14 @@ const Dashboard = ({ onSignOut }) => {
 
       <Drawer.Body>
         <h3>Hey, {profile.name} </h3>
+        <ProviderBlock />
         <Divider />
         <EditableInput
-        name="nickname"
-         initialValue={profile.name} onSave = {onSave} 
-        label  = {<h6 className='mb-2' >Nickname</h6>} />
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
